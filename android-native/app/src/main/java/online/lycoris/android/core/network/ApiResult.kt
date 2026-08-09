99cb0aab3fe114df2d67dd0cf94d030df7ced9de
Nora <eleanor_1018@outlook.com>
2026-08-10 01:48:04 +0800

    feat (style) :手机端ui ux 重构，蓝粉色主题

--- android-native/app/src/main/java/online/lycoris/android/core/network/ApiResult.kt
+++
@@ -1,6 +0,0 @@
-package online.lycoris.android.core.network
-
-sealed interface ApiResult<out T> {
-    data class Success<T>(val value: T) : ApiResult<T>
-    data class Failure(val status: Int?, val message: String) : ApiResult<Nothing>
-}
